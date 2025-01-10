9fc20d3c559ec4abd4a32233416378e1b0943ddf
belinda17 <belinda9817@gmail.com>
2025-01-10 16:06:50 +0800

    第一次javascript課

---
+++ app.java.js
@@ -0,0 +1,61 @@
+// alert("目前的時間")
+// const currentDate= new Date();
+// const currentHours=currentDate.getHours();
+// const currentMinutes=currentDate.getMinutes();
+// const currentSeconds=currentDate.getSeconds();
+// alert(`現在是 ${currentHours} 時 ${currentMinutes}分 ${currentSeconds}秒` );
+
+// let data=prompt("說明文字","Hi")
+// alert(`prompt`)
+
+// function addNumbers(a,b){const result=a+b; return result}
+//
+// const num1 = 10;
+// const num2 = 20;
+// const sum= addNumbers(num1,num2)
+// console.log("總和", sum)
+
+// function time(){
+// const currentDate= new Date();
+// const currentHours=currentDate.getHours();
+// const currentMinutes=currentDate.getMinutes();
+// const currentSeconds=currentDate.getSeconds();
+// alert(`現在是 ${currentHours} 時 ${currentMinutes}分 ${currentSeconds}秒` );}
+
+// let myName=prompt("請輸入姓名")
+// function greet(name){let result="Hello" + name; alert(result)}
+// greet(myName);
+
+
+//  let myDay=prompt("今天星期幾");
+// switch (myDay) {
+//     case "Monday":alert("workout")
+//         break;
+//
+//     case "Tuesday":alert("eat")
+//         break;
+//        default:
+//            alert("wrong");
+//            break;}
+
+
+// let myscore= prompt("輸入分數")
+// if (myscore>=90){alert("A")}
+// else if (myscore>=80){alert("B")}
+// else {alert("E")}
+
+//12小時制
+// function time() {
+//     const currentDate = new Date();
+//     let currentHours = currentDate.getHours();
+//     const currentMinutes = currentDate.getMinutes();
+//     const currentSeconds = currentDate.getSeconds();
+//     let format = "上午";
+//
+//     if (currentHours >= 12) {
+//         format = "下午";
+//         currentHours = currentHours - 12;
+//     }
+//     alert(`現在是 ${(format)} ${currentHours} 時 ${currentMinutes}分 ${currentSeconds}秒`);
+// }
+//   time();
